3aac96258ded862dd2c6db2106323c5d879c35a9
Jangsoodlor <celica2548@gmail.com>
2022-05-08 19:53:45 +0700

    Should be working now
    (hopefully)

--- form.js
+++ form.js
@@ -1,3 +1,18 @@
-console.log('hello')
+function submitForm() {
+    
+    const name = document.getElementById('name').value;
+    const url = document.getElementById('url').value;
+    const time = document.getElementById('time').value;
+    const password = document.getElementById('password').value;
+    console.log(name, url, time, password);
 
+    if (time === '') {
+        window.open(`https://short.fakepng.com/create?name=${name}&link=${url}&password=${password}/`, '_blank');
+    }
+
+    else if (time !== ''){
+        window.open(`https://short.fakepng.com/create?name=${name}&link=${url}&time=${time}&password=${password}/`, '_blank');
+    }
+    
+}
 
