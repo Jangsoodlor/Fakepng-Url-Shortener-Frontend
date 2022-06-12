62a19eb06f5df6ad67a44712d10a370ace9109af
Jangsoodlor <celica2548@gmail.com>
2022-06-12 21:52:54 +0700

    add the ablility to open your link via clicking.
    Co-authored-by: Fakepng <contact@fakepng.com>

--- form.js
+++ form.js
@@ -6,13 +6,20 @@ function submitForm() {
     const password = document.getElementById('password').value;
     console.log(name, url, time, password);
 
-    if (time === '') {
-        window.open(`https://short.fakepng.com/create?name=${name}&link=${url}&password=${password}`, '_blank');
-    }
-
-    else if (time !== ''){
-        window.open(`https://short.fakepng.com/create?name=${name}&link=${url}&time=${time}&password=${password}`, '_blank');
+    if (!time) {
+        const link = `https://short.fakepng.com/create?name=${name}&link=${url}&password=${password}`
+        changeText(link);
+        // window.open(link, '_blank')
     }
     
+    else {
+        const link = `https://short.fakepng.com/create?name=${name}&link=${url}&time=${time}&password=${password}`
+        changeText(link);
+        // window.open(link, '_blank');
+    }
+}
+
+const changeText = (link = 'https://jangsoodlor.github.io/Fakepng-Url-Shortener-Frontend/') => {
+    document.getElementById('thomas').innerHTML = `<a href=${link}>Click me</a>`;
 }
 
