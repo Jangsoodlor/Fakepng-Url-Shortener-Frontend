997684388e1b20f5289d2f081278959bf5b057f2
Jangsoodlor <celica2548@gmail.com>
2022-06-18 14:44:33 +0700

    ink shortener link is now functioning properly
    Co-authored-by: Fakepng <contact@fakepng.com>

--- form.js
+++ form.js
@@ -1,5 +1,9 @@
+const createLink = (link, name) => {
+    fetch(link);
+    changeText(name);
+};
+
 function submitForm() {
-    
     const name = document.getElementById('name').value;
     const url = document.getElementById('url').value;
     const time = document.getElementById('time').value;
@@ -8,18 +12,15 @@ function submitForm() {
 
     if (!time) {
         const link = `https://short.fakepng.com/create?name=${name}&link=${url}&password=${password}`
-        changeText(link);
-        // window.open(link, '_blank')
+        createLink(link, name);
     }
     
     else {
         const link = `https://short.fakepng.com/create?name=${name}&link=${url}&time=${time}&password=${password}`
-        changeText(link);
-        // window.open(link, '_blank');
+        createLink(link, name);
     }
 }
 
-const changeText = (link = 'https://jangsoodlor.github.io/Fakepng-Url-Shortener-Frontend/') => {
-    document.getElementById('thomas').innerHTML = `<a href=${link}>Click me</a>`;
+const changeText = (name) => {
+    document.getElementById('thomas').innerHTML = `<a href=https://short.fakepng.com/${name}>Click here to open your link </a>`;
 }
-
