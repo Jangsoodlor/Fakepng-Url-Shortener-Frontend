235cf9115843e81b261baf9a512fdb470b748517
Jangsoodlor <celica2548@gmail.com>
2022-08-06 17:34:47 +0700

    Add delete function
    yay

--- form.js
+++ form.js
@@ -3,6 +3,11 @@ const createLink = (link, name) => {
     changeText(name);
 };
 
+const deleteLinkAKtion = (deleteLink, deleteName) => {
+    fetch(deleteLink);
+    confirmText(deleteName);
+}
+
 function submitForm() {
     const name = document.getElementById('name').value;
     const url = document.getElementById('url').value;
@@ -21,6 +26,18 @@ function submitForm() {
     }
 }
 
+function deleteForm(){
+    const deleteName = document.getElementById('name').value;
+    const deletePassword = document.getElementById('password').value;
+
+    const deleteLink = `https://short.fakepng.com/remove?name=${deleteName}&password=${deletePassword}`
+    deleteLinkAKtion(deleteLink, deleteName);
+}
+
 const changeText = (name) => {
     document.getElementById('thomas').innerHTML = `<a href=https://short.fakepng.com/${name}>Click here to open your link </a>`;
+}
+
+const confirmText = (deleteName) => {
+    document.getElementById('thomas').innerHTML = `${deleteName} is deleted`;
 }
